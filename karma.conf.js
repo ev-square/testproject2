4b2cc3f218d9a20874707222905e17c4a89528e5
Manu <manuvarkey1987@gmail.com>
2023-05-05 02:36:31 +0530

    test2 (#1)
    Co-authored-by: manumelakathu <manu.melakathu@aurionpro.com>

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: ''123456,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),

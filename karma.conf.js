d1c4688c8f3c46dcc84dc400e3a03685effd2908
manumelakathu <manu.melakathu@aurionpro.com>
2023-05-10 15:54:55 +0530

    demo commit 3

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
 
-    basePath: 'abcd123456',
+    basePath: 'abcd12345634rtergtfgfd',
 
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
